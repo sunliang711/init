c6036d37ca58c2bb35d61fe76ed95235af256446
sunliang711 <sunliang711@163.com>
2023-12-08 15:55:53 +0800

    Config: in OnceCell

--- bin/rust_tmpl/src/config/utils.rs
+++ bin/rust_tmpl/src/config/utils.rs
@@ -6,20 +6,17 @@ use figment::{
     Figment,
 };
 use once_cell::sync::OnceCell;
-use std::sync::{RwLock, RwLockReadGuard};
-static CONFIG: OnceCell<RwLock<Config>> = OnceCell::new();
+
+static CONFIG: OnceCell<Config> = OnceCell::new();
 
 impl Config {
-    pub fn get() -> RwLockReadGuard<'static, Config> {
-        let c = CONFIG.get_or_init(|| {
-            let config = Figment::from(Serialized::defaults(Config::default()))
+    pub fn get() -> &'static Config {
+        CONFIG.get_or_init(|| {
+            Figment::from(Serialized::defaults(Config::default()))
                 .merge(Toml::file(consts::CONFIG_TOML_FILE))
                 .merge(Env::prefixed(consts::CONFIG_ENV_PREFIX))
                 .extract()
-                .expect(consts::ERR_EXTRACT_FIGMENT);
-
-            RwLock::new(config)
-        });
-        c.read().expect(consts::ERR_GET_CONFIG)
+                .expect(consts::ERR_EXTRACT_FIGMENT)
+        })
     }
 }
