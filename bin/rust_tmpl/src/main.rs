da76d383affd3fcb63546e4418da2b6d257551dc
sunliang711 <sunliang711@163.com>
2023-12-31 17:41:49 +0800

    RUST_LOG env

--- bin/rust_tmpl/src/main.rs
+++ bin/rust_tmpl/src/main.rs
@@ -10,7 +10,7 @@ fn main() -> anyhow::Result<()> {
     // embed build info to the binary
     utils::build_info();
 
-    utils::init::init_env(true, true);
+    utils::init::init_env(true, true, "info");
 
     // example: get config
     let cfg = crate::config::Config::get();
--- bin/rust_tmpl/src/utils/init.rs
+++ bin/rust_tmpl/src/utils/init.rs
@@ -1,10 +1,13 @@
-pub fn init_env(dotenv: bool, env_logger: bool) {
+pub fn init_env(dotenv: bool, env_logger: bool, rust_log_value: &str) {
     if dotenv {
         // load .env file
         dotenv::dotenv().ok();
     }
 
     if env_logger {
+        if !rust_log_value.is_empty() {
+            std::env::set_var("RUST_LOG", rust_log_value)
+        }
         // init env_logger
         env_logger::init();
     }
