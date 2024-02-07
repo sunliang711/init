6771bf3b878a1671d03773ef95225d255809a83f
sunliang711 <sunliang711@163.com>
2024-02-07 10:57:16 +0800

    init tracing

--- bin/rust_tmpl/src/main.rs
+++ bin/rust_tmpl/src/main.rs
@@ -10,11 +10,12 @@ fn main() -> anyhow::Result<()> {
     // embed build info to the binary
     utils::build_info();
 
-    utils::init::init_env(true, true, "info");
+    // utils::init::init_env(true, true, "info");
+    utils::init_tracing();
 
     // example: get config
     let cfg = crate::config::Config::get();
-    println!("config: {cfg}");
+    tracing::info!("config: {cfg}");
 
     Ok(())
 }
