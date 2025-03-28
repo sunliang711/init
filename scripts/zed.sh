eb30a2d31b6c252a2080c8fa484653b7eac40eca
eagle <sunliang711@163.com>
2025-03-28 10:55:15 +0800

    link zed snippets

--- scripts/zed.sh
+++ scripts/zed.sh
@@ -260,103 +260,103 @@ LOG_LEVELS=("FATAL" "ERROR" "WARNING" "INFO" "SUCCESS" "DEBUG")
 MAX_LEVEL_LENGTH=0
 
 for level in "${LOG_LEVELS[@]}"; do
-  len=${#level}
-  if (( len > MAX_LEVEL_LENGTH )); then
-    MAX_LEVEL_LENGTH=$len
-  fi
+    len=${#level}
+    if ((len > MAX_LEVEL_LENGTH)); then
+        MAX_LEVEL_LENGTH=$len
+    fi
 done
-MAX_LEVEL_LENGTH=$((MAX_LEVEL_LENGTH+2))
+MAX_LEVEL_LENGTH=$((MAX_LEVEL_LENGTH + 2))
 
 # 日志级别名称填充
 pad_level() {
-  printf "%-${MAX_LEVEL_LENGTH}s" "[$1]"
+    printf "%-${MAX_LEVEL_LENGTH}s" "[$1]"
 }
 
 # 打印带颜色的日志函数
 log() {
-  local level="$(echo "$1" | awk '{print toupper($0)}')" # 转换为大写以支持大小写敏感
-  shift
-  local message="$@"
-  local padded_level=$(pad_level "$level")
-  local timestamp=$(date "+%Y-%m-%d %H:%M:%S")
-  case "$level" in
+    local level="$(echo "$1" | awk '{print toupper($0)}')" # 转换为大写以支持大小写敏感
+    shift
+    local message="$@"
+    local padded_level=$(pad_level "$level")
+    local timestamp=$(date "+%Y-%m-%d %H:%M:%S")
+    case "$level" in
     "FATAL")
-      if [ $LOG_LEVEL -ge $LOG_LEVEL_FATAL ]; then
-        echo -e "${RED}${BOLD}[$timestamp] $padded_level${NC} $message${NORMAL}"
-        exit 1
-      fi
-      ;;
+        if [ $LOG_LEVEL -ge $LOG_LEVEL_FATAL ]; then
+            echo -e "${RED}${BOLD}[$timestamp] $padded_level${NC} $message${NORMAL}"
+            exit 1
+        fi
+        ;;
 
     "ERROR")
-      if [ $LOG_LEVEL -ge $LOG_LEVEL_ERROR ]; then
-        echo -e "${RED}${BOLD}[$timestamp] $padded_level${NC} $message${NORMAL}"
-      fi
-      ;;
+        if [ $LOG_LEVEL -ge $LOG_LEVEL_ERROR ]; then
+            echo -e "${RED}${BOLD}[$timestamp] $padded_level${NC} $message${NORMAL}"
+        fi
+        ;;
     "WARNING")
-      if [ $LOG_LEVEL -ge $LOG_LEVEL_WARNING ]; then
-        echo -e "${YELLOW}${BOLD}[$timestamp] $padded_level${NC} $message${NORMAL}"
-      fi
-      ;;
+        if [ $LOG_LEVEL -ge $LOG_LEVEL_WARNING ]; then
+            echo -e "${YELLOW}${BOLD}[$timestamp] $padded_level${NC} $message${NORMAL}"
+        fi
+        ;;
     "INFO")
-      if [ $LOG_LEVEL -ge $LOG_LEVEL_INFO ]; then
-        echo -e "${BLUE}${BOLD}[$timestamp] $padded_level${NC} $message${NORMAL}"
-      fi
-      ;;
+        if [ $LOG_LEVEL -ge $LOG_LEVEL_INFO ]; then
+            echo -e "${BLUE}${BOLD}[$timestamp] $padded_level${NC} $message${NORMAL}"
+        fi
+        ;;
     "SUCCESS")
-      if [ $LOG_LEVEL -ge $LOG_LEVEL_SUCCESS ]; then
-        echo -e "${GREEN}${BOLD}[$timestamp] $padded_level${NC} $message${NORMAL}"
-      fi
-      ;;
+        if [ $LOG_LEVEL -ge $LOG_LEVEL_SUCCESS ]; then
+            echo -e "${GREEN}${BOLD}[$timestamp] $padded_level${NC} $message${NORMAL}"
+        fi
+        ;;
     "DEBUG")
-      if [ $LOG_LEVEL -ge $LOG_LEVEL_DEBUG ]; then
-        echo -e "${CYAN}${BOLD}[$timestamp] $padded_level${NC} $message${NORMAL}"
-      fi
-      ;;
+        if [ $LOG_LEVEL -ge $LOG_LEVEL_DEBUG ]; then
+            echo -e "${CYAN}${BOLD}[$timestamp] $padded_level${NC} $message${NORMAL}"
+        fi
+        ;;
     *)
-      echo -e "${NC}[$timestamp] [$level] $message${NORMAL}"
-      ;;
-  esac
+        echo -e "${NC}[$timestamp] [$level] $message${NORMAL}"
+        ;;
+    esac
 }
 
 # 设置日志级别的函数
 set_log_level() {
-  local level="$(echo "$1" | awk '{print toupper($0)}')"
-  case "$level" in
+    local level="$(echo "$1" | awk '{print toupper($0)}')"
+    case "$level" in
     "FATAL")
-      LOG_LEVEL=$LOG_LEVEL_FATAL
-      ;;
+        LOG_LEVEL=$LOG_LEVEL_FATAL
+        ;;
     "ERROR")
-      LOG_LEVEL=$LOG_LEVEL_ERROR
-      ;;
+        LOG_LEVEL=$LOG_LEVEL_ERROR
+        ;;
     "WARNING")
-      LOG_LEVEL=$LOG_LEVEL_WARNING
-      ;;
+        LOG_LEVEL=$LOG_LEVEL_WARNING
+        ;;
     "INFO")
-      LOG_LEVEL=$LOG_LEVEL_INFO
-      ;;
+        LOG_LEVEL=$LOG_LEVEL_INFO
+        ;;
     "SUCCESS")
-      LOG_LEVEL=$LOG_LEVEL_SUCCESS
-      ;;
+        LOG_LEVEL=$LOG_LEVEL_SUCCESS
+        ;;
     "DEBUG")
-      LOG_LEVEL=$LOG_LEVEL_DEBUG
-      ;;
+        LOG_LEVEL=$LOG_LEVEL_DEBUG
+        ;;
     *)
-      echo "无效的日志级别: $1"
-      ;;
-  esac
+        echo "无效的日志级别: $1"
+        ;;
+    esac
 }
 
 # 显示帮助信息
 show_help() {
-  echo "Usage: $0 [-l LOG_LEVEL] <command>"
-  echo ""
-  echo "Commands:"
-  for cmd in "${COMMANDS[@]}"; do
-    echo "  $cmd"
-  done
-  echo ""
-  echo "Options:"
-  echo "  -l LOG_LEVEL  Set the log level (FATAL ERROR, WARNING, INFO, SUCCESS, DEBUG)"
+    echo "Usage: $0 [-l LOG_LEVEL] <command>"
+    echo ""
+    echo "Commands:"
+    for cmd in "${COMMANDS[@]}"; do
+        echo "  $cmd"
+    done
+    echo ""
+    echo "Options:"
+    echo "  -l LOG_LEVEL  Set the log level (FATAL ERROR, WARNING, INFO, SUCCESS, DEBUG)"
 }
 
 # ------------------------------------------------------------
@@ -367,29 +367,45 @@ link() {
     dest=$HOME/.config/zed
     settingFile="${dest}/settings.json"
     keymapFile="${dest}/keymap.json"
+    snippetDir="${dest}/snippets"
 
     sourceSettingFile="${this}/../softlinks/zed/settings.json"
     sourceKeymapFile="${this}/../softlinks/zed/keymap.json"
+    sourceSnippetDir="${this}/../softlinks/zed/snippets"
 
-    if [ ! -d "$dest}" ];then
+    if [ ! -d "$dest}" ]; then
         log INFO "create ${dest}.."
-        mkdir -p "${dest}" || { log ERROR "create ${dest} failed"; exit 1; }
+        mkdir -p "${dest}" || {
+            log ERROR "create ${dest} failed"
+            exit 1
+        }
     fi
 
-    if [ -e "$settingFile" ];then
+    # link settings.json
+    if [ -e "$settingFile" ]; then
         log INFO "${settingFile} already exists, backup it.."
         cp "${settingFile}" "${settingFile}.old"
     fi
     log INFO "link ${sourceSettingFile} to ${settingFile}.."
     ln -sf "${sourceSettingFile}" "${settingFile}"
 
-    if [ -e "$keymapFile" ];then
+    # link keymap.json
+    if [ -e "$keymapFile" ]; then
         log INFO "${keymapFile} already exists, backup it.."
         cp "${keymapFile}" "${keymapFile}.old"
     fi
-
     log INFO "link ${sourceKeymapFile} to ${keymapFile}.."
     ln -sf "${sourceKeymapFile}" "${keymapFile}"
+
+    # link snippets
+    if [ -d "$snippetDir" ]; then
+        log INFO "${snippetDir} already exists, backup it.."
+        cp -r "${snippetDir}" "${snippetDir}.old"
+        log INFO "remove ${snippetDir}.."
+        rm -rf "${snippetDir}"
+    fi
+    log INFO "link ${sourceSnippetDir} to ${snippetDir}.."
+    ln -sf "${sourceSnippetDir}" "${snippetDir}"
 
 }
 
@@ -397,38 +413,38 @@ link() {
 
 # 解析命令行参数
 while getopts ":l:" opt; do
-  case ${opt} in
-    l )
-      set_log_level "$OPTARG"
-      ;;
-    \? )
-      show_help
-      exit 1
-      ;;
-    : )
-      echo "Invalid option: $OPTARG requires an argument" 1>&2
-      show_help
-      exit 1
-      ;;
-  esac
+    case ${opt} in
+    l)
+        set_log_level "$OPTARG"
+        ;;
+    \?)
+        show_help
+        exit 1
+        ;;
+    :)
+        echo "Invalid option: $OPTARG requires an argument" 1>&2
+        show_help
+        exit 1
+        ;;
+    esac
 done
 # NOTE: 这里全局使用了OPTIND，如果在某个函数中也使用了getopts，那么在函数的开头需要重置OPTIND (OPTIND=1)
-shift $((OPTIND -1))
+shift $((OPTIND - 1))
 
 # 解析子命令
 command=$1
 shift
 
 if [[ -z "$command" ]]; then
-  show_help
-  exit 0
+    show_help
+    exit 0
 fi
 
 case "$command" in
-  help)
+help)
     show_help
     ;;
-  *)
+*)
     ${command} "$@"
     ;;
 esac
