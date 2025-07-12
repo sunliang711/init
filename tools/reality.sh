a888f0b3dedb459f3d69ee657ed43f0f86e75094
eagle <sunliang711@163.com>
2025-07-13 07:15:06 +0800

    save publicKey for client

--- tools/reality.sh
+++ tools/reality.sh
@@ -151,6 +151,7 @@ function config_xray(){
                     "$website"
                 ],
                 "privateKey": "$privateKey",
+                "publicKey": "$publicKey",
                 "minClientVer": "",
                 "maxClientVer": "",
                 "maxTimeDiff": 0,
